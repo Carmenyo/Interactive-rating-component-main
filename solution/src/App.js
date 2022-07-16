681d798ec875db0b52fa5c740a5f8aeb51c62d97
Carmenyo <carmencastroyo@gmail.com>
2022-07-16 21:26:58 +0200

    final push

--- solution/src/App.js
+++ solution/src/App.js
@@ -3,16 +3,17 @@ import star from "./images/icon-star.svg"
 import thanks from "./images/illustration-thank-you.svg"
 
 function App() {
-  
+  const [submitted, setSubmitted] = useState(false)
+  const [items, setitems] = useState ("")
 
 
   const Button = ({number}) => {
-    return <button className="button-number">{number}</button>
+    return <button onClick={()=> setitems(number)} className="button-number">{number}</button>
   }
   return (
     <>
-    <div className="wrapper">
-  
+  {!submitted && (  
+  <div className="wrapper">
   <img src={star} alt="star" className="icon-star" />
   <h2 className="title">How did we do?</h2>
   <p className="paragraph">Please let us know how we did with your support request. All feedback is appreciated 
@@ -25,20 +26,20 @@ function App() {
   <li><Button number={5}/></li>
   </ul>
   <div className="button-wrapper">
-  <button className="button-rating">Submit</button>
+  <button onClick={()=> setSubmitted (true)} className="button-rating">Submit</button>
   </div>
-    </div>
-    <ThankYou/>
+    </div>) }
+    {submitted && <ThankYou items={items} setSubmitted={setSubmitted}/>}
     </>
   );
 }
 
-const ThankYou = () => {
+const ThankYou = ({items}) => {
 return (
 <div className="thanks__wrapper">
   <img className="thanks__image" src={thanks} alt="Thanks" />
   <div className="selected__wrapper">
-  <p className="thanks__selected">You selected number out of 5</p>
+  <p className="thanks__selected">You selected {items} out of 5</p>
   </div>
 <h2 className="thanks__title">Thank you!</h2>
 <p class="thanks__paragraph">  We appreciate you taking the time to give a rating. If you ever need more support, 
