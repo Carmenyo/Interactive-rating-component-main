e0b9ac2b4859bef2e20e7aa1eed5a1dad9c7db14
Carmenyo <carmencastroyo@gmail.com>
2022-07-16 18:54:04 +0200

    layout finished

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,27 +1,48 @@
+import {useState} from "react"
 import star from "./images/icon-star.svg"
+import thanks from "./images/illustration-thank-you.svg"
 
 function App() {
-  const Button = () => {
-    return <button></button>
+  
+
+
+  const Button = ({number}) => {
+    return <button className="button-number">{number}</button>
   }
   return (
+    <>
     <div className="wrapper">
-  <img src={star} alt="star" className="bg-gray-700 p-2 rounded-full" />
+  
+  <img src={star} alt="star" className="icon-star" />
   <h2 className="title">How did we do?</h2>
   <p className="paragraph">Please let us know how we did with your support request. All feedback is appreciated 
   to help us improve our offering!</p>
   <ul className="rating-list">
-  <li><button className="bg-gray-700 p-2 rounded-full">1</button></li>
-  <li><button className="bg-gray-700 p-2 rounded-full">2</button></li>
-  <li><button className="bg-gray-700 p-2 rounded-full">3</button></li>
-  <li><button className="bg-gray-700 p-2 rounded-full">4</button></li>
-  <li><button className="bg-gray-700 p-2 rounded-full">5</button></li>
+  <li><Button number={1}/></li>
+  <li><Button number={2}/></li>
+  <li><Button number={3}/></li>
+  <li><Button number={4}/></li>
+  <li><Button number={5}/></li>
   </ul>
   <div className="button-wrapper">
   <button className="button-rating">Submit</button>
   </div>
     </div>
+    <ThankYou/>
+    </>
   );
 }
 
+const ThankYou = () => {
+return (
+<div className="thanks__wrapper">
+  <img className="thanks__image" src={thanks} alt="Thanks" />
+  <div className="selected__wrapper">
+  <p className="thanks__selected">You selected number out of 5</p>
+  </div>
+<h2 className="thanks__title">Thank you!</h2>
+<p class="thanks__paragraph">  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch!</p>
+</div>
+)}
 export default App;
